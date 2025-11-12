fb74243a919131de0c7526b27383ba9e036e0004
Mehedi Hasan <mehedihasan6002@gmail.com>
2025-11-12 19:52:43 +0600

    Price removed

--- one2many_bulk_delete/__manifest__.py
+++ one2many_bulk_delete/__manifest__.py
@@ -18,8 +18,6 @@
     """,
     'author': 'MD. MEHEDI HASAN REAL',
     'website': 'https://www.linkedin.com/in/mehedihasanreal/',
-    'price': 5.0,
-    'currency': 'USD',
     'license': 'LGPL-3',
     'depends': ['web', 'sale', 'purchase'],
     'data': [
